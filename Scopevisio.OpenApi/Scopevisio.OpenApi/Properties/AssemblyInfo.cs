7fe1bf53adea81abad6d0684ef27e778f09b0a81
Jochen Wezel <jwezel@compumaster.de>
2020-05-06 12:17:43 +0200

    added alternative library created by VS generator for REST-API Clients

---
+++ Scopevisio.OpenApi/Scopevisio.OpenApi/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Scopevisio OpenAPI Library")]
+[assembly: AssemblyDescription("A library generated from a OpenAPI doc")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CompuMaster")]
+[assembly: AssemblyProduct("CompuMaster Scopevisio OpenAPI Library")]
+[assembly: AssemblyCopyright("2020 CompuMaster GmbH")]
+[assembly: AssemblyTrademark("camm")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("672274f7-238e-445c-a791-1682c8c3a3fb")]
+
+[assembly: AssemblyVersion("0.0.1")]

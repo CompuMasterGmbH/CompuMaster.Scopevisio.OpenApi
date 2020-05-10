eeb4551604dacddeffe9ce776c2aa8bfdc0d9b7a
Jochen Wezel <jwezel@compumaster.de>
2020-05-10 15:02:23 +0200

    added NuGet gallery push feature

--- src/CompuMaster.Scopevisio.OpenApi/Properties/AssemblyInfo.cs
+++ src/CompuMaster.Scopevisio.OpenApi/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Scopevisio OpenAPI Library")]
-[assembly: AssemblyDescription("A library generated from a OpenAPI doc")]
+[assembly: AssemblyDescription("Client access library for the OpenScope API https://www.openscope.de/ (REST web API interface provided by Scopevisio AG, https://www.scopevisio.com/), library generated from a Swagger/OpenAPI doc at https://appload.scopevisio.com/rest/swagger.json")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CompuMaster")]
 [assembly: AssemblyProduct("CompuMaster Scopevisio OpenAPI Library")]
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("651A72B5-BC80-4838-8528-0A81F2B8E57B")]
 
-[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]

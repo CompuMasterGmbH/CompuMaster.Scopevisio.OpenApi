4efd0e82d392e7ed3e33ee68695067537c9dc017
Jochen Wezel <jwezel@compumaster.de>
2020-05-11 16:46:25 +0200

    Update Program.cs

--- ConsoleSampleApp/Program.cs
+++ ConsoleSampleApp/Program.cs
@@ -11,32 +11,31 @@ namespace ConsoleSampleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter username: ");
-            string username = Console.ReadLine();
-            Console.WriteLine("Please enter customer no.: ");
-            string customerno = Console.ReadLine();
-            Console.WriteLine("Please enter password: ");
-            string password = Console.ReadLine();
-
-            username = "report@bierdeckel-kalkulation.de";
-            password = "PWe}VA_6eqX6(W8$~d)PVHe&";
-            customerno = "2036092";
+            System.Console.WriteLine("PLEASE NOTE: Following user input will be buffered in directory " + System.IO.Path.GetTempPath());
+            string username = InputLine("username");
+            string customerno = InputLine("customer no.");
+            string password = InputLine("password");
 
             OpenScopeApiClient OpenScopeClient = new OpenScopeApiClient();
             try
             {
+                //Authorize initially with user credentials -> recieved access token will be saved for following requests
                 OpenScopeClient.AuthorizeWithUserCredentials(username, customerno, password);
                 System.Console.WriteLine("Authorization successful, future requests can be executed with access token:\n" + OpenScopeClient.Token.AccessToken);
                 System.Console.WriteLine();
 
+                //A 1st API method call for getting API version info (authorized by access token from initial request)
                 CompuMaster.Scopevisio.OpenApi.Model.Version VersionResult = OpenScopeClient.AdditionalApi.GetVersionWithHttpInfo().Data;
                 System.Console.WriteLine("Interface version=" + VersionResult.ToString());
                 System.Console.WriteLine();
 
+                //A 2nd API method call for getting HelloWorld data (authorized by access token from initial request)
                 CompuMaster.Scopevisio.OpenApi.Model.Hello HelloResult = OpenScopeClient.AdditionalApi.HelloJsonWithHttpInfo().Data;
                 System.Console.WriteLine("Hello world=" + HelloResult.HelloMessage);
                 System.Console.WriteLine();
 
+                //A 3rd API method call for getting HelloWorld data (authorized by access token from initial request)
+                //Demonstration of async requests
                 Task<CompuMaster.Scopevisio.OpenApi.Model.Hello> t = HelloTask(OpenScopeClient);
                 t.Wait();
                 System.Console.WriteLine("Async hello world=" + t.Result.HelloMessage);
@@ -63,6 +62,32 @@ namespace ConsoleSampleApp
         {
             ApiResponse<CompuMaster.Scopevisio.OpenApi.Model.Hello> HelloResponse = await openScopeClient.AdditionalApi.HelloJsonAsyncWithHttpInfo();
             return HelloResponse.Data;
+        }
+
+        /// <summary>
+        /// Ask user for field data and buffer them in local files
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        static string InputLine(string fieldName)
+        {
+            string BufferFile = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "~Buffer.Sample." + fieldName.GetHashCode().ToString() + ".tmp");
+            string DefaultValue;
+            if (System.IO.File.Exists(BufferFile))
+                DefaultValue = System.IO.File.ReadAllText(BufferFile);
+            else
+                DefaultValue = "";
+
+            Console.WriteLine("Please enter " + fieldName + " [" + DefaultValue + "]: ");
+            string UserInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(UserInput))
+                return DefaultValue;
+            else
+            {
+                System.IO.File.WriteAllText(BufferFile, UserInput);
+                return UserInput;
+            }
         }
     }
 }
